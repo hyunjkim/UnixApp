4dbb56a3f34cf5817c5dbefa06d072bec81f7319
hyunjkim <hkim6501@gmail.com>
2016-09-24 18:00:31 -0400

    updated :poop:

--- app/src/main/java/nyc/c4q/hyun/unix/MainActivity.java
+++ app/src/main/java/nyc/c4q/hyun/unix/MainActivity.java
@@ -1,49 +1,44 @@
 package nyc.c4q.hyun.unix;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     public static String TAG = "MainActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.image);
+        myImageView.setImageResource(R.drawable.unixcctv_logo_converted);
 
         // Capture our button from layout
         Button button = (Button)findViewById(R.id.button);
         Button buttonTwo = (Button)findViewById(R.id.button2);
 
         // Register the onClick listener with the implementation above
-        button.setOnClickListener(mCorkyListener);
-        buttonTwo.setOnClickListener(mCorkyListener);
-
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "clicked! ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "clicked! ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
-
-        Context context = getApplicationContext();
-        CharSequence text = "clicked";
-        int duration = Toast.LENGTH_SHORT;
-
-        public void onClick(View v) {
-            // do something when the button is clicked
-//            Toast toast = Toast.makeText(context, text, duration);
-//            toast.setText(text);
-            // ... Or as a single line:
-            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-        }
-
-    };
-
 
     @Override
     protected void onStart() {
@@ -81,8 +76,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d(TAG,"onPause");
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
 }
